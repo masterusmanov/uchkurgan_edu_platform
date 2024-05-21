83bed8e0e5b09f1fb8efac8baaac0f48515770f5
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-05-21 11:49:08 +0500

    edu_platform

---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { resolve } from 'path';
+import { JwtModule } from '@nestjs/jwt';
+// import { UserModule } from './user/user.module';
+// import { CategoryModule } from './category/category.module';
+// import { ProductsModule } from './products/products.module';
+// import { PaymentsModule } from './payments/payments.module';
+// import { PaymentMethodModule } from './payment_method/payment_method.module';
+// import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, 'static'),
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+      logging: false,
+    }),
+    JwtModule,
+    // UserModule,
+    // CategoryModule,
+    // ProductsModule,
+    // PaymentsModule,
+    // PaymentMethodModule,
+    // OrdersModule
+  ],
+  providers: [],
+  exports: [JwtModule],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const PORT = process.env.PORT || 1987;
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS TEST')
+    .setDescription('REST API')
+    .setVersion('1.0.0')
+    .addTag('NodeJS, NestJS, Postgres, Sequalize')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`${PORT}th port is running`);
+  });
+}
+bootstrap();
