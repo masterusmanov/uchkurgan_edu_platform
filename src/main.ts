097f39bb49bc9393d5631216406fb5b8e03846f1
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-06-20 06:58:24 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const PORT = process.env.PORT || 1987;
   app.use(cookieParser());
